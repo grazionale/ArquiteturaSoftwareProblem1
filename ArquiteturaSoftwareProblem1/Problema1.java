67d52ef8b0487bc15f3eedc8d429d7d0cd380197
grazionale <gabrielgrazionale@hotmail.com>
2018-03-14 20:39:30 -0300

    Primeiro commit

---
+++ ArquiteturaSoftwareProblem1/Problema1.java
@@ -0,0 +1,33 @@
+package arquiteturasoftwareproblema1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArquiteturaSoftwareProblema1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> nomes = new ArrayList<String>();
+
+        do {
+            System.out.println("Deseja listar ou entrar? (listar/entrar)");
+            String x = sc.nextLine();
+
+            if (x.equals("listar")) {
+                if(!nomes.isEmpty()) {
+                    for (int i = 0; i < nomes.size(); i++) {
+                        System.out.println(nomes.get(i) + "\n");
+                    }
+                } else {
+                    System.out.println("Lista vazia \n\n");
+                }
+                
+            } else {
+                System.out.println("Digite o nome: ");
+                nomes.add(sc.nextLine());
+            }
+        } while (true);
+
+    }
+
+}
